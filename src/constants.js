8f58443983679e31f9f953bc7b696d583bc1ddb4
Alberto Martinez <betomartinezs1323@gmail.com>
2024-10-15 04:13:42 -0400

    Remove node:path package

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,2 @@
-import path from 'node:path';
-
 export const SERVER_URI = 'localhost:50051';
-export const PROTO_PATH = path.resolve('./src/calculator.proto');
+export const PROTO_PATH = './src/calculator.proto';
